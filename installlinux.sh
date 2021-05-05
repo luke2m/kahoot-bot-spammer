a840e04031a985c0b80951b08628725648b3cd36
luke2m <66685476+luke2m@users.noreply.github.com>
2021-05-05 11:12:34 -0400

    Update installlinux.sh

--- installlinux.sh
+++ installlinux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mv node_modules.a/ node_modules/
+cp -r node_modules/node_modules.a/* node_modules
 apt install -y nano nodejs npm
 
 echo "Just open spamkahoot.js with your favorite text editor and change the kahootCode variable to your kahoot game pin"
